65ea5e551a15d9d4365f172ef5d2300484cd1b46
combikms <combikms@naver.com>
2024-08-12 22:55:59 +0900

    :sparkles: lombok 설치, DB 데이터 출력 실험

--- src/main/java/com/kangcombi/shop/ItemController.java
+++ src/main/java/com/kangcombi/shop/ItemController.java
@@ -1,13 +1,24 @@
 package com.kangcombi.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
     @GetMapping("/list")
     String listpage(Model model) {
+        List<Item> result = itemRepository.findAll();
+        System.out.println(result.get(0).price);
+        System.out.println(result.get(0).title);
+
         model.addAttribute("name", "kang");
         return "list.html";
     }
