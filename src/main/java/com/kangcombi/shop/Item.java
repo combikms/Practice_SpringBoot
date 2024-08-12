de0f528ef8fb2b515eaae0c76f1526625b632508
combikms <combikms@naver.com>
2024-08-12 22:26:14 +0900

    :sparkles: Entity

---
+++ src/main/java/com/kangcombi/shop/Item.java
@@ -0,0 +1,12 @@
+package com.kangcombi.shop;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Item {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String title;
+    public Integer price;
+
+}
