f3c1c1d8933b4a989dcebff30074b1383fae1383
combikms <combikms@naver.com>
2024-08-13 18:53:21 +0900

    :sparkles: Insert 기능, navbar UI

--- src/main/java/com/kangcombi/shop/ItemController.java
+++ src/main/java/com/kangcombi/shop/ItemController.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +22,20 @@ public class ItemController {
         model.addAttribute("items", result);
         return "list.html";
     }
+
+    @GetMapping("/write")
+    String writepage(Model model) {
+        return "write.html";
+    }
+
+    @PostMapping("/add")
+    String addPost(@RequestParam String title, Integer price) {
+        Item item = new Item();
+        item.title = title;
+        item.price = price;
+        itemRepository.save(item);
+        return "redirect:/list";
+    }
+
 
 }
